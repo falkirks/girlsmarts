6dddebbd43817f7f696a1dd23bf42f564b4dd0e3
Falkirks <me@falkirks.com>
2016-12-20 19:42:47 -0800

    Initial commit

---
+++ routes/buses.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var rest = require('restler');
+
+router.get('/', function(req, res) {
+  rest.get(API_ENDPOINT + 'buses?apikey=' + process.env.TRANSLINK_KEY).on('complete', function(data) {
+    if(data.Buses == null){
+      res.json({"error": "An error occured."});
+      return;
+    }
+    data = data.Buses.Bus;
+    for(var i = 0; i < data.length; i++){
+      data[i] = {
+        'number': trimBus(data[i].RouteNo[0]),
+        'direction': data[i].Direction[0],
+        'destination': data[i].Destination[0],
+        'lat': data[i].Latitude[0],
+        'long': data[i].Longitude[0]
+      };
+    }
+    res.json(data);
+
+  });
+});
+
+router.get('/:stopId', function(req, res) {
+  rest.get(API_ENDPOINT + 'buses?apikey=' + process.env.TRANSLINK_KEY + '&stopNo=' + req.params.stopId).on('complete', function(data) {
+    if(data.Buses == null){
+      res.json({"error": "An error occured. Maybe the stop doesn't exist?"});
+      return;
+    }
+    data = data.Buses.Bus;
+    for(var i = 0; i < data.length; i++){
+      data[i] = {
+        'number': trimBus(data[i].RouteNo[0]),
+        'direction': data[i].Direction[0],
+        'destination': data[i].Destination[0],
+        'lat': data[i].Latitude[0],
+        'long': data[i].Longitude[0]
+      };
+    }
+    res.json(data);
+
+  });
+});
+
+module.exports = router;
---
+++ routes/stops.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var rest = require('restler');
+
+var cache = {};
+
+/* GET users listing. */
+router.get('/:stopId', function(req, res) {
+  if(cache[req.params.stopId] !== undefined){
+    addEstimates(cache[req.params.stopId], function(data){
+      res.json(data);
+    });
+  }
+  else {
+    rest.get(API_ENDPOINT + 'stops/' + req.params.stopId + '?apikey=' + process.env.TRANSLINK_KEY).on('complete', function (data) {
+      if (data.Stop == null) {
+        res.json({"error": "An error occured. Maybe the stop doesn't exist?"});
+      }
+
+      // GENERATE OUR STATIC DATA
+      data = {
+        'number': data.Stop.StopNo[0],
+        'name': data.Stop.Name[0],
+        'accessible': data.Stop.WheelchairAccess[0] === 1,
+        'street': data.Stop.OnStreet[0],
+        'lat': data.Stop.Latitude[0],
+        'long': data.Stop.Longitude[0],
+        'buses': data.Stop.Routes[0].split(',').map(function (bus) {
+          return bus.trim();
+        }).map(trimBus)
+      };
+
+      cache[data.number] = data;
+
+      addEstimates(data, function(data){
+        res.json(data);
+      });
+
+    });
+  }
+
+});
+router.get('/:stopId/times', function(req, res) {
+  addEstimates({'number': req.params.stopId}, function(data){
+    res.json(data.times);
+  })
+});
+
+function addEstimates(old, cb){
+  rest.get(API_ENDPOINT + 'stops/' + old.number + '/estimates?apikey=' + process.env.TRANSLINK_KEY).on('complete', function (data) {
+    data = data.NextBuses.NextBus;
+    var out = [];
+    for(var i = 0; i < data.length; i++){
+      console.log("hey");
+      var route = {
+        'number': trimBus(data[i].RouteNo[0]),
+        'direction': data[i].Direction[0]
+      };
+      var next = data[i].Schedules[0].Schedule;
+      for(var j = 0; j < next.length; j++){
+          out.push({
+            'number': route.number,
+            'direction': route.direction,
+            'destination': next[j].Destination[0],
+            'minutes': Number(next[j].ExpectedCountdown[0])
+          });
+      }
+    }
+
+    out.sort(function(a, b){
+      return a.minutes - b.minutes;
+    });
+    old.times = out;
+    cb(old);
+  });
+}
+
+module.exports = router;
