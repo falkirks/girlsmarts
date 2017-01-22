33501be4a242866ccf34bc4da288548f2fd52000
Falkirks <me@falkirks.com>
2017-01-21 20:54:03 -0800

    Added tutorial tracking

--- routes/fun.js
+++ routes/fun.js
@@ -1,9 +1,20 @@
 module.exports = function (io) {
   var express = require('express');
   var router = express.Router();
+  var path = require('path');
 
   router.get('/', function (req, res) {
     res.render('fun', {title: 'Girlsmarts Programming'});
+  });
+  router.get('/track', function (req, res) {
+    res.sendFile(path.resolve('public/t.gif'));
+    io.emit('tutorial', {
+      index: req.query.index,
+      total: req.query.total,
+      completed: req.query.completed == "true",
+      time: req.query.time,
+      anonid: req.query.anonid
+    });
   });
   return router;
 };
